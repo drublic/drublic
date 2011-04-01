35d506ac6952ad68ab89ad5916144643c4a8f7b8
drublic <hans@sa-designz.de>
2011-04-02 00:26:34 +0200

    Add main functions
    Signed-off-by: drublic <hans@sa-designz.de>

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
        Remove this if you use the .htaccess -->
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-  <title>Source Access Designz</title>
+  <title>@drublic</title>
   <meta name="description" content="">
   <meta name="author" content="">
 
@@ -38,90 +38,135 @@
 
   <div id="container">
     <header>
+      <nav>
+        <ul>
+          <li><a href="home">Home</a></li>
+          <li><a href="imprint">Imprint</a></li>
+          <li><a href="contact">Contact</a></li>
+        </ul>
+      </nav>
+      
       <hgroup>
-        <a href="/">Source Access Designz</a>
+        <a href="/">@drublic</a>
       </hgroup>
     </header>
     
     <div id="main" role="main">
       <section class="flipthemes">
-        <h2>Flipthemes</h2>
-        <a href="http://flipthemes.de/" target="_blank">Flipthemes</a>
+        <h2><a href="http://flipthemes.de/" target="_blank">Flipthemes</a></h2>
         
         <div class="feed">
-          &hellip;
+          <ul>
+            <li>This stream is empty&hellip;</li>
+          </ul>
         </div>
       </section>
       
       <section class="pagetimer">
-        <h2>Pagetimer</h2>
-        <a href="http://pagetimer.de/" target="_blank">Pagetimer</a>
+        <h2><a href="http://pagetimer.de/" target="_blank">Pagetimer</a></h2>
         
         <div class="feed">
           <ul>
+            <div class="loader"></div>
           </ul>
-        
         </div>
       </section>
       
       <section class="twitter">
-        <h2>Twitter</h2>
-        <a href="http://twitter.com/drublic" target="_blank">Twitter</a>
+        <h2><a href="http://twitter.com/drublic" target="_blank">Twitter</a></h2>
         
         <div class="feed">
           <ul>
-            <?php
-              // Es wird ein Array angelegt, in dem dann die wichtigsten Daten gespeichert werden.
-              $data = 'http://www.twitter.com/status/user_timeline/pagetimer.json?count=10';
-              
-              // Es wird geprüft ob die Php Funktion simplexml_load_file existiert.
-              if(!function_exists('file_get_contents'))
-              	die ('{ "error" : "Ihre PHP Version unterstützt die Funktion simplexml_load_file leider nicht." }');
-              
-              
-              
-              // Als nächstes wird die User Timeline eingelesen oder ein Fehler ausgegeben.
-              if(!@$twitter=file_get_contents($data)) :
-                // Fehlermeldung
-              	print '{ "error" : "Es ist ein Fehler bei der Verbindung zu Twitter aufgetreten." }';
-              else :
-                // Ausgabe des Usernames und der Nachricht.
-                $twitter = json_decode($twitter);
-                
-                foreach ($twitter as $tweet) :
-                  print '<li>';
-                  print '<p>' . $tweet->text . '</p>';
-                  ?>
-                    <a href="http://twitter.com/drublic/status/<?php print $tweet->id; ?>" target="_blank" class="date"><?php print $tweet->created_at; ?></a>
-                  <?php
-                  print '</li>';
-                endforeach;
-              endif;
-
-            ?>
+            <div class="loader"></div>
           </ul>
-
         </div>
       </section>
       
       <section class="projects">
-        <h2>Latest Projects</h2>
-        <a href="">Latest Projects</a>
+        <h2><a href="">Latest Projects</a></h2>
         
         <div class="feed">
+          <ul>
+            <li>
+              <span class="front">
+                <img src="img/jsfiddle.png" alt="">
+                <h4>Google Chrome App - jsFiddle</h4>
+              </span>
+              <span class="back">
+                <p><a href="http://jsfiddle.net" target="_blank">jsFiddle</a> is a pretty useful social coding tool. This is an app for Google Chrome which works like a bookmark.</p>
+                <p><a href="https://chrome.google.com/webstore/detail/combhimnlhkmpfebbfbccnkgopecnoem" target="_blank">Visit</a> Chrome Web Store to view the app and download it.</p>
+              </span>
+            </li>
           
+          </ul>
         </div>
       </section>
       
-      <section class="small tumblr">
-        <h2>Tumblr</h2>
-        <a href="http://drublic.tumblr.com/" target="_blank">Tumblr</a>
+      <section class="tumblr">
+        <h2><a href="http://drublic.tumblr.com/" target="_blank">Tumblr</a></h2>
         
         <div class="feed">
-        
+          <ul>
+            <div class="loader"></div>
+          </ul>
         </div>
       </section>
+      
+
     </div>
+    
+    <!--! Imprint -->
+    <section class="legal">
+      <a href="#!/close" class="close">&times;</a>
+      
+      <h2>Legal Notice</h2>
+      
+      <div class="clearfix">
+        <div class="col_50">
+          <h3>Source Access Designz</h3>
+          <p>
+            Obere Bachstra&szlig;e 15<br>
+            35606 Solms<br>
+            Germany
+          </p>
+          <p>
+            <a href="http://drublic.de">drublic.de</a><br>  
+            <a href="mailto:info@drublic.de">info@drublic.de</a><br>
+            +49 176 44508482<br>
+          </p>
+        </div>
+        
+        <div class="col_50">
+          <h3>Contentswise Responsibility</h3>
+          <p>Hans Christian Reinl</p>
+          
+          <p>Finanzamt Wetzlar<br>
+          Tax ID: 039 860 01625</p>
+        </div>
+      </div>
+      
+      
+      <h3>Some things&hellip;</h3>
+
+Haftung für Inhalte
+Die Inhalte der Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte kann Source Access Designz jedoch keine Gewähr übernehmen.
+Weiterhin übernimmt Source Access Designz trotz sorgfältiger inhaltlicher Kontrolle keine Haftung für die Inhalte externer Links. Für den Inhalt der verlinkten Seiten sind ausschließlich deren Betreiber verantwortlich.
+Alle hier präsentierten Inhalte unterliegen dem urheberrechtlichen Schutz.
+
+Datenschutz
+Wir (Source Access Designz) nehmen den Schutz Ihrer persönlichen Daten sehr ernst. Unser Wunsch ist es, dass Sie sich beim Besuch unserer Internetseiten sicher fühlen.
+
+Bei Ihrem Besuch auf unserer Internetseite werden uns grundsätzlich nur anonyme Daten, wie zum Beispiel die Seite, von der aus Sie uns besuchen, Ihr Verbindungsanbieter und die Seiten, die Sie auf unserer Präsenz aufgerufen haben, übermittelt. Wir nutzen diese Informationen ausschließlich zu statistischen Zwecken und zur Verbesserung unseres Service und unserer Internetseite.
+
+Die Erhebung, Verarbeitung und Nutzung personenbezogener Daten ist im Rahmen dieser Internetseite an einigen Stellen - zum Beispiel für die Kontaktaufnahme über das Kontaktfomular - nötig. Diese Daten werden nur auf der Basis Ihrer bewusst gemachten Angaben und nur entsprechend den jeweils geltenden Vorschriften verarbeitet und genutzt. Die Nutzung schließt - soweit zulässig - eine elektronische Speicherung mit ein (vorwiegend im Bezug auf E-Mail-Datenverkehr).
+
+Benutzerstatistiken mit Piwik
+Diese Website benutzt Piwik, einen freien Webanalysedienst, beruhend auf einer freien Programmierung (OpenSource). Piwik verwendet sogenannte "Cookies", Textdateien, die auf Ihrem Computer gespeichert werden und die eine Analyse der Benutzung der Website durch Sie ermöglicht. Ihre Daten werden von unserem Server nicht an Dritte weitergegeben.
+Sie können die Installation der Cookies durch eine entsprechende Einstellung Ihrer Browsersoftware verhindern.
+Durch die Nutzung dieser Website erklären Sie sich mit der Verarbeitung der über Sie erhobenen Daten durch unser System und zu dem benannten Zweck einverstanden.
+
+Auf Anforderung teilen wir Ihnen gerne schriftlich entsprechend dem geltenden Recht mit, ob und welche persönlichen Daten über Sie bei uns gespeichert sind. Wenn Sie Fragen hinsichtlich der Verarbeitung Ihrer persönlichen Daten haben, können Sie sich direkt an uns wenden.
+    </section>
     <footer>
 
     </footer>
@@ -137,6 +182,7 @@
 
   <!-- scripts concatenated and minified via ant build script-->
   <script src="js/plugins.js"></script>
+  <script src="js/twitter-text.js"></script>
   <script src="js/script.js"></script>
   <!-- end scripts-->
 
--- js/script.js
+++ js/script.js
@@ -3,6 +3,58 @@
 */
 
 
+! function ($, window, document, undefined) {
+
+
+// Change Links in Nav
+$('nav').find('a').each( function () {
+  $(this).attr('href', '#!/' + $(this).attr('href'))
+})
+
+// Click one of those
+.click( function () {
+  var i = 0;
+  
+  // Slide 'em down
+  ! function slideDownSec () {
+    
+    // Do it for each section
+    if (i < $('#main').find('section').size()) {
+      if (!$('#main').find('section').eq(i).hasClass('legal'))
+        $('#main').find('section').eq(i).animate({ top: $('#main').height() }, function () {
+          $(this).fadeOut();
+        });
+      
+      i++;
+      
+      setTimeout(slideDownSec, 100);
+    }
+    else {
+      $('.legal').delay(500).fadeIn();
+    }
+  } ();
+});
+
+
+// Click on close
+$('.close').click( function () {
+  $('.legal').fadeOut(function () {
+    var i = 0;
+    ! function slideUpSec () {
+      // Do it for each section
+      if (i < $('#main').find('section').size()) {
+        if (!$('#main').find('section').eq(i).hasClass('legal'))
+          $('#main').find('section').eq(i).show() .animate({ top: 0 });
+        
+        i++;
+        
+        setTimeout(slideUpSec, 100);
+      }
+      
+    } ();
+  });
+  
+});
 
 
 
@@ -10,11 +62,114 @@
 
 
 
+/* Date-routine */
+function date (date) {
+  date = new Date (date);
+	var date_str  = date.getDate() + '.' + parseInt(date.getMonth() + 1) + '.' + date.getFullYear();
+	    date_str += ' - ' + date.getHours() + ':' + ( (date.getMinutes() <= 9) ? '0' + date.getMinutes() : date.getMinutes() );
+  
+  return date_str;
+}
 
 
 
 
 
+
+// Request latest Blog-Posts from Pagetimer-Blog
+$.get('http://blog.pagetimer.de/api/read/json?num=10&filter=text', function (data) {
+  log(data);
+  $('.pagetimer .feed ul').html('');
+  
+  var body = '';
+  $.each( data.posts, function (key, value) {
+    body = value['regular-body'].substr(0, 100);
+    
+    $('.pagetimer .feed ul').append('<li style="display: none;"><h4><a href="' + value['url-with-slug'] + '" target="_blank">' + 
+      value['regular-title'] + '</a></h4><span>' + twttr.txt.autoLink(body) + ' ' +
+      '<a href="' + value['url-with-slug'] + '" target="_blank">read more&hellip;</a>' +
+      '<a href="' + value['url-with-slug'] + '" target="_blank" class="date">' + date(value['date']) + '</a></li>');
+    
+    $('.pagetimer .feed ul').find('li:last').delay(key * 100).animate({ 'opacity': 'toggle' });
+  });
+}, 'jsonp');
+
+
+
+// Request latest Tweets
+$.get('http://www.twitter.com/status/user_timeline/drublic.json?count=10', function (data) {
+  log(data);
+  $('.twitter .feed ul').html('');
+
+  $.each( data, function (key, value) {
+    $('.twitter .feed ul').append('<li style="display: none;">' + twttr.txt.autoLink(value['text']) + ' ' +
+      '<a href="http://twitter.com/drublic/status/' + value['id'] + '" target="_blank" class="date">' + date(value['created_at']) + '</a></li>');
+    
+    $('.twitter .feed ul').find('li:last').delay(key * 100).animate({ 'opacity': 'toggle' });
+  });
+}, 'jsonp');
+
+
+
+
+
+// Request latest Blog-Posts from Tumblr-Blog
+$.get('http://drublic.tumblr.com/api/read/json?num=5&filter=text', function (data) {
+  log(data);
+  $('.tumblr .feed ul').html('');
+  
+  var body = '', output = '';
+  $.each( data.posts, function (key, value) {
+    output = '<li style="display: none;">';
+    
+    
+    // Texts
+    if (value['regular-body'] !== undefined) {
+      output += '<h4><a href="' + value['url-with-slug'] + '" target="_blank">' + value['regular-title'] + '</a></h4>';
+  
+      body = value['regular-body'].substr(0, 100);
+      output += '<span>' + twttr.txt.autoLink(body) + ' ' +
+        '<a href="' + value['url-with-slug'] + '" target="_blank">read more&hellip;</a>';
+    }
+    
+    // Photo
+    else if (value['photo-url-500'] !== undefined) {
+      output += '<a href="' + value['photo-url-500'] + '" target="_black"><img src="' + value['photo-url-250'] + '" alt=""></a>';
+    }
+    
+    else return;
+    output += '<a href="' + value['url-with-slug'] + '" target="_blank" class="date">' + date(value['date']) + '</a></li>';
+    $('.tumblr .feed ul').append(output)
+    
+    $('.tumblr .feed ul').find('li:last').delay(key * 100).animate({ 'opacity': 'toggle' });
+  });
+}, 'jsonp');
+
+
+
+
+
+
+/* Calculate height of Cols */
+! function () {
+  var i = 0;
+  ! function calcHeight () {
+    // Do it for each section
+    if (i < $('#main').find('section').size()) {
+      if (!$('#main').find('section').eq(i).hasClass('legal'))
+        $('#main').find('section').eq(i).height($('html').height() - $('#main').offset().top);
+      
+      i++;
+      calcHeight();
+    }
+    
+  } ();
+
+
+} ();
+
+
+} (jQuery, window, undefined);
 
 
 
