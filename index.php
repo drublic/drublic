926c2aa98fddf60a2bbc41561d80fa9f2b233adf
drublic <hans@sa-designz.de>
2011-05-17 23:27:11 +0200

    Update jQuery to 1.6.1
    Signed-off-by: drublic <hans@sa-designz.de>

--- index.php
+++ index.php
@@ -233,8 +233,8 @@
   <!-- JavaScript at the bottom for fast page loading -->
 
   <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if necessary -->
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.js"></script>
-  <script>window.jQuery || document.write("<script src='<?php base_url(); ?>js/libs/jquery-1.6.0.min.js'>\x3C/script>")</script>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.js"></script>
+  <script>window.jQuery || document.write("<script src='<?php base_url(); ?>js/libs/jquery-1.6.1.min.js'>\x3C/script>")</script>
 
 
   <!-- scripts concatenated and minified via ant build script-->
