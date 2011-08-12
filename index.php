410af9b2c2ab90262aa0976454f40448a19651c5
drublic <hans@sa-designz.de>
2011-08-12 21:23:56 +0200

    Better JS for Piwik
    Signed-off-by: drublic <hans@sa-designz.de>

--- index.php
+++ index.php
@@ -281,10 +281,10 @@
   <!-- end scripts-->
 
   <!-- Piwik --> 
-  <script type="text/javascript">
+  <script>
   var pkBaseURL = (("https:" == document.location.protocol) ? "https://drublic.de/piwik/" : "http://drublic.de/piwik/");
-  document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
-  </script><script type="text/javascript">
+  document.write('<script src="' + pkBaseURL + 'piwik.js"><\/script>');
+  </script><script>
   try {
   var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 1);
   piwikTracker.trackPageView();
