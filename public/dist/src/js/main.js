9ba757f4eed6c61f5f01044295cc00471ba0cbfe
Hans Christian Reinl <info@drublic.de>
2014-11-10 01:24:59 +0100

    v3.2.0

--- public/dist/src/js/main.js
+++ public/dist/src/js/main.js
@@ -36,4 +36,63 @@
   $(document).on('click', '.navigation__toggle', function () {
     $('.navigation').toggleClass('navigation--visible');
   });
+
+  /**
+   * Form
+   */
+  $('.form__nojs').addClass('form__nojs--hidden');
+
+  var resetForm = function ($form) {
+    $form[0].reset();
+  };
+
+  var errorForm = function ($form, data) {
+    $('.contact--thanks__error').show();
+    $('.contact--thanks__success').hide();
+
+    $form.find('.form__message').show();
+
+    $('.form__save')
+      .attr('disabled', false)
+      .val('Submit again');
+
+    $.each(data.FieldErrors, function (element) {
+      console.log(element.ErrorText);
+    });
+
+    // Normalize Thanks page
+    $(document).on('click', '.contact--thanks__back', function () {
+      $('.contact--thanks__error').hide();
+      $('.contact--thanks__success').show();
+    });
+  };
+
+  $('.form').submit(function () {
+    var $form = $(this);
+    var formData = $form.serializeArray();
+    var success = true;
+
+    $('.form__save')
+      .attr('disabled', true)
+      .val('Sending...');
+
+    $.post('?file=form', formData, function (data) {
+      if (data.success === 1) {
+        resetForm($form);
+      } else if (data.ErrorText) {
+        errorForm($form, data);
+        success = false;
+      }
+    }, 'json');
+
+    setTimeout(function () {
+
+      // Prevent thanks page from being shown if form is already submitted
+      if (success) {
+        window.location.hash = 'contact/thanks';
+      }
+    }, 500);
+
+    return false;
+  });
 }());
