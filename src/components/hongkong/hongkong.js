3445e9e2691db5e904fb59174c3a30743df1651d
Hans Christian Reinl <info@drublic.de>
2015-01-28 21:35:08 +0100

    Update to latest components

--- src/components/hongkong/hongkong.js
+++ src/components/hongkong/hongkong.js
@@ -2,9 +2,16 @@
  * Parallax scrolling
  */
 (function ($) {
-    var $scrollTop = $('[data-parallax-top]');
-    var $scrollBottom = $('[data-parallax-bottom]');
+
+    // Setting for the plugin
+    var settings = {};
+
+    // Get elements
+    var $scrollTop;
+    var $scrollBottom;
+
     var scrollPosition = 0;
+    var ticking = false;
 
     /**
      * Get the factor attribute for each
@@ -14,53 +21,110 @@
         var i;
 
         for (i = 0; i < $scrollTop.length; i++) {
-            $scrollTop[i].factor = parseInt($scrollTop[i].getAttribute('data-parallax-factor') || 4, 10);
+            $scrollTop[i].factor = parseFloat($scrollTop[i].getAttribute('data-parallax-factor') || settings.factor, 10);
         }
 
         for (i = 0; i < $scrollBottom.length; i++) {
-            $scrollBottom[i].factor = parseInt($scrollBottom[i].getAttribute('data-parallax-factor') || 4, 10);
+            $scrollBottom[i].factor = parseFloat($scrollBottom[i].getAttribute('data-parallax-factor') || settings.factor, 10);
         }
     };
 
+    var _isElementInViewport = function ($element) {
+        var offsetTop = $element.offset().top;
+
+        return (
+            (scrollPosition <= offsetTop + $element.height()) &&
+            ($(window).height() + scrollPosition >= offsetTop)
+        );
+    };
 
     /**
      * Callback for rAF
      * @return {void}
      */
     var _callback = function () {
-        var scroll = window.pageYOffset;
+        var visible;
         var i;
+        var rectObject = 0;
 
-        /**
-         * Eject if update isn't needed
-         */
-        if (scrollPosition === scroll) {
-            for (i = 0; i < $scrollTop.length; i++) {
-                $scrollTop[i].style.transform = 'translateY(0) translateZ(0)';
-            }
+        // Don't do anything if we've scrolled to the top
+        if (scrollPosition <= 0) {
+            $scrollTop.css({ 'transform': 'translateY(0) translateZ(0)' });
+            $scrollBottom.css({ 'transform': 'translateY(0) translateZ(0)' });
 
-            window.requestAnimationFrame(_callback);
+            ticking = false;
 
-            return false;
+            return;
         }
 
         for (i = 0; i < $scrollTop.length; i++) {
-            $scrollTop[i].style.transform = 'translateY(' + Math.floor(scroll / $scrollTop[i].factor) + 'px) translateZ(0)';
+            rectObject = $scrollTop[i].getBoundingClientRect();
+            visible = _isElementInViewport($($scrollTop[i]).parent());
+
+            $scrollTop[i].style.visibility = visible ? 'visible' : 'hidden';
+
+            if (visible) {
+                $($scrollTop[i]).css({ transform: 'translateY(' + Math.floor(rectObject.top / $scrollTop[i].factor) + 'px) translateZ(0)' });
+            }
         }
 
         for (i = 0; i < $scrollBottom.length; i++) {
-            $scrollBottom[i].style.transform = 'translateY(' + Math.floor(scroll / ($scrollBottom[i].factor * -1)) + 'px) translateZ(0)';
+            rectObject = $scrollBottom[i].getBoundingClientRect();
+            visible = _isElementInViewport($($scrollBottom[i]).parent());
+
+            $scrollBottom[i].style.visibility = visible ? 'visible' : 'hidden';
+
+            if (visible) {
+                $($scrollBottom[i]).css({ transform: 'translateY(' + Math.floor(rectObject.top / ($scrollBottom[i].factor * -1)) + 'px) translateZ(0)' });
+            }
         }
 
-        window.requestAnimationFrame(_callback);
+        // allow further rAFs to be called
+        ticking = false;
     };
 
     /**
-     * Init
+     * Events
      */
-    if ($scrollTop.length || $scrollBottom.length) {
-        _generateFactor();
-        window.requestAnimationFrame(_callback);
-    }
+    $(document).on('hongkong:refresh', _callback);
+
+    /**
+     * Init as jQuery plugin
+     */
+    $.hongkong = function (options) {
+
+        // Options
+        settings = $.extend({
+            factor: 4,
+            mobile: false,
+            mediaQuery: '(max-width: 42em)',
+            selectorBottom: '[data-parallax-bottom]',
+            selectorTop: '[data-parallax-top]'
+        }, options);
+
+        // Set elements
+        $scrollTop = $(settings.selectorTop);
+        $scrollBottom = $(settings.selectorBottom);
+
+        if ($scrollTop.length || $scrollBottom.length) {
+            _generateFactor();
+
+            // only listen for scroll events
+            $(window).on('scroll', function () {
+                scrollPosition = Math.max($('body').scrollTop(), $('html').scrollTop());
+
+                if (!settings.mobile && window.matchMedia && window.matchMedia(settings.mediaQuery).matches) {
+                    return false;
+                }
+
+                if (!ticking) {
+                    window.requestAnimationFrame(_callback);
+
+                    ticking = true;
+                }
+            });
+        }
+
+    };
 
 }(jQuery));
