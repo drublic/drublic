292ea432f210ad546989ce5d657b43018db15fdc
Hans Christian Reinl <info@drublic.de>
2015-07-20 23:43:21 +0200

    Add scroll to top element, styling for text, icons & code

--- public/dist/_/core.js
+++ public/dist/_/core.js
@@ -707,6 +707,29 @@ void 0===c?d&&"get"in d&&null!==(e=d.get(a,b))?e:(e=n.find.attr(a,b),null==e?voi
   });
 
   $header.find('.header__background').append(pattern.svg());
+
+  /**
+   * Scroll top element
+   */
+  (function () {
+    var $window = $(window);
+
+    var onScroll = function () {
+      var scrolled = $window.scrollTop();
+      var windowHeight = $window.height();
+
+      $('[data-scroll-top]').toggleClass('is-hidden', scrolled < windowHeight);
+    };
+
+    $window.on('scroll', onScroll);
+
+    $(document).on('click', '[data-scroll-top]', function () {
+      $('html, body').animate({
+        'scrollTop': 0
+      }, 400);
+    });
+  }());
+
 }(jQuery));
 
 (function () {
--- src/js/main.js
+++ src/js/main.js
@@ -102,4 +102,27 @@
   });
 
   $header.find('.header__background').append(pattern.svg());
+
+  /**
+   * Scroll top element
+   */
+  (function () {
+    var $window = $(window);
+
+    var onScroll = function () {
+      var scrolled = $window.scrollTop();
+      var windowHeight = $window.height();
+
+      $('[data-scroll-top]').toggleClass('is-hidden', scrolled < windowHeight);
+    };
+
+    $window.on('scroll', onScroll);
+
+    $(document).on('click', '[data-scroll-top]', function () {
+      $('html, body').animate({
+        'scrollTop': 0
+      }, 400);
+    });
+  }());
+
 }(jQuery));
