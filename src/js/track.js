eb5cf6c330b16c465a3771915b491ad6128b823e
Hans Christian Reinl <info@drublic.de>
2016-12-17 09:47:41 +0100

    Add tracking in header

--- src/js/track.js
+++ src/js/track.js
@@ -2,20 +2,7 @@
 
   'use strict';
 
-  // Google Universal Analytics
-
-  // More information about the Google Universal Analytics:
-  // https://developers.google.com/analytics/devguides/collection/analyticsjs/
-  // http://mathiasbynens.be/notes/async-analytics-snippet#universal-analytics
-  /* jshint ignore:start */
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-  /* jshint ignore:end */
-
-  ga('create', 'UA-41497561-1', 'auto');
-  ga('send', 'pageview');
+  var ga = window.ga
 
   // Track events
   // https://developers.google.com/analytics/devguides/collection/analyticsjs/events
