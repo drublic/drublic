d64590c32d14ca11bab610c611ac7da18f574e12
Hans Christian Reinl <info@drublic.de>
2018-03-05 23:10:39 +0100

    Try to fix webpack build with babel-loader and index.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,22 +5,16 @@ const pkg = require('./package')
 module.exports = {
   mode: 'production',
   entry: {
-    main: [
-      path.resolve(__dirname, 'node_modules/jquery/dist/jquery.js'),
-      path.resolve(__dirname, 'node_modules/css-modal/modal.js'),
-      path.resolve(__dirname, 'src/js/transition.js'),
-      path.resolve(__dirname, 'src/js/zoom.js'),
-      path.resolve(__dirname, 'src/js/effects.js'),
-      path.resolve(__dirname, 'src/js/form.js'),
-      path.resolve(__dirname, 'src/js/toc.js'),
-      path.resolve(__dirname, 'src/js/main.js'),
-      path.resolve(__dirname, 'src/js/scroll-top.js'),
-      path.resolve(__dirname, 'src/js/testimonial.js'),
-      path.resolve(__dirname, 'src/js/track.js'),
-    ],
+    main: path.resolve(__dirname, 'src/js/index.js'),
   },
   output: {
     path: path.resolve(__dirname, 'public/dist/', pkg.version),
     filename: `c.js`
+  },
+
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'babel-loader' }
+    ]
   },
 }
