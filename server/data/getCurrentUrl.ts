76b6ae57db813912527a45f2604d72659cb63809
Hans Christian Reinl <info@drublic.de>
2018-11-11 21:38:04 +0100

    Use only path for current Url

--- server/data/getCurrentUrl.ts
+++ server/data/getCurrentUrl.ts
@@ -2,7 +2,7 @@ import { PageInterface } from "../../pages";
 import getBaseUrl from "./getBaseUrl";
 
 const getCurrentUrl = (page: PageInterface): string => {
-  return `${getBaseUrl()}${page.action}`;
+  return page.action;
 };
 
 export default getCurrentUrl;
