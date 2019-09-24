d992a50e62d8c0f3920e7d8e0f8d3c065617741d
Hans Christian Reinl <info@drublic.de>
2019-09-24 10:08:28 +0200

    Try to fix wd posts

--- server/lib/getWdPosts.ts
+++ server/lib/getWdPosts.ts
@@ -3,13 +3,17 @@ const Parser = require("rss-parser");
 
 const getWdPosts = async (): Promise<any> => {
   const parser = new Parser();
-  const feed: any = await parser.parseURL('https://workingdraft.de/feed/');
+  try {
+    const feed: any = await parser.parseURL('https://workingdraft.de/feed/');
 
-  return feed.items.splice(0, 5).map((item: any) => ({
-    title: item.title,
-    url: item.link,
-    date: format(new Date(item.isoDate), "dd.MM.yyyy"),
-  }));
+    return feed.items.splice(0, 5).map((item: any) => ({
+      title: item.title,
+      url: item.link,
+      date: format(new Date(item.isoDate), "dd.MM.yyyy"),
+    }));
+  } catch (error) {
+    return [];
+  }
 };
 
 export default getWdPosts;
