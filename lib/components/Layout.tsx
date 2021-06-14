cf4793c836843032a6a38cee96b591e6c69dd2c9
Hans Christian Reinl <info@drublic.de>
2021-06-14 21:28:39 +0200

    Update google tag manager include

--- lib/components/Layout.tsx
+++ lib/components/Layout.tsx
@@ -26,17 +26,14 @@ const Layout = ({
       <div
         dangerouslySetInnerHTML={{
           __html: `
-          <script async>
-            (function (i, s, o, g, r, a, m) {
-              i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-                (i[r].q = i[r].q || []).push(arguments)
-              }, i[r].l = 1 * new Date(); a = s.createElement(o),
-                m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-            })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+          <!-- Global site tag (gtag.js) - Google Analytics -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-45228510-1"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-            ga('create', 'UA-41497561-1', 'auto');
-            ga('set', 'anonymizeIp', true);
-            ga('send', 'pageview');
+            gtag('config', 'UA-45228510-1');
           </script>`,
         }}
       />
