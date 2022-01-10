0d2e1cfa88a5174fbcbae8e79341f4fadc32693b
Hans Christian Reinl <info@drublic.de>
2022-01-10 20:29:52 +0100

    Some work on 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,9 +3,20 @@ import Layout from "../lib/components/Layout";
 
 const Error = () => (
   <Layout title="Imprint">
-    <main id="content" className="main container" role="main">
-      <h1>Page Not Found</h1>
-      <p>Sorry, but the page you were trying to view does not exist.</p>
+    <main
+      id="content"
+      className="main container"
+      role="main"
+      style={{
+        display: "flex",
+        alignItems: "center",
+        minHeight: "calc(100vh - 180px - 200px)",
+      }}
+    >
+      <div>
+        <h1>Page Not Found</h1>
+        <p>Sorry, but the page you were trying to view does not exist.</p>
+      </div>
     </main>
   </Layout>
 );
