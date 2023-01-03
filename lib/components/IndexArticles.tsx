efc8c8d2afa972b6a2edd36c2087c49b8ed92616
Hans Christian Reinl <info@drublic.de>
2023-01-03 20:29:38 +0100

    Fixes to new layout

--- lib/components/IndexArticles.tsx
+++ lib/components/IndexArticles.tsx
@@ -1,51 +1,52 @@
-import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import useSWR from "swr";
-import fetcher from "../utils/fetcher";
 import ArticleTeaser from "./ArticleTeaser";
 import ArticleTeaserSmall from "./ArticleTeaserSmall";
 
 const IndexArticles = ({ posts }) => {
   const withImages = posts.slice(1, 7).filter(({ image }) => !!image);
-  const teaserLength = withImages.length % 3 === 0 ? 9 : 8;
+  const teaserLength = withImages.length % 2 === 0 ? 8 : 7;
 
   return (
-    <section className="container">
-      <h2>From the Blog</h2>
+    <section>
+      <div className="container">
+        <h2>From the Blog</h2>
 
-      {posts
-        ?.slice(0, 1)
-        .map(({ title, slug, date, abstract, image, tags }) => (
-          <ArticleTeaser
-            key={slug}
-            title={title}
-            date={date}
-            slug={slug}
-            abstract={abstract}
-            tags={tags}
-            variant="small"
-            image={image}
-          />
-        ))}
+        {posts
+          ?.slice(0, 1)
+          .map(({ title, slug, date, abstract, image, tags }) => (
+            <ArticleTeaser
+              key={slug}
+              title={title}
+              date={date}
+              slug={slug}
+              abstract={abstract}
+              tags={tags}
+              variant="small"
+              image={image}
+            />
+          ))}
+      </div>
 
-      <ul className="posts__list posts__list--no-divider posts__list--no-top">
-        {posts?.slice(1, teaserLength).map(({ title, date, slug, image }) => (
-          <ArticleTeaserSmall
-            key={slug}
-            title={title}
-            date={date}
-            slug={slug}
-            image={image}
-          />
-        ))}
-      </ul>
+      <div className="container container--large">
+        <ul className="posts__list posts__list--no-divider posts__list--no-top">
+          {posts?.slice(1, teaserLength).map(({ title, date, slug, image }) => (
+            <ArticleTeaserSmall
+              key={slug}
+              title={title}
+              date={date}
+              slug={slug}
+              image={image}
+            />
+          ))}
+        </ul>
 
-      <p className="text--centered text--padded">
-        <Link href="/blog/" className="button">
-          Read more on my Blog
-        </Link>
-      </p>
+        <p className="text--centered text--padded">
+          <Link href="/blog/" className="button">
+            Read more on my Blog
+          </Link>
+        </p>
+      </div>
     </section>
   );
 };
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -17,29 +17,71 @@ const Blog: FunctionComponent<Props> = ({ posts, tag, tags }) => {
     <Layout title="Blog">
       <main
         id="main"
-        className="main container"
+        className="main"
         role="main"
         itemScope
         itemType="http://schema.org/Blog"
       >
-        {tag && (
-          <>
-            <p className="typography--h2">
-              Posts matching <span className="tag">{tag}</span>
-            </p>
-
-            <p className="typography--h3">
-              {posts.length} {posts.length > 1 ? "results" : "result"}
-            </p>
-
-            <hr />
-          </>
-        )}
-
-        {posts
-          ?.slice(0, 1)
-          .map(({ title, slug, date, abstract, tags, image }) => (
+        <div className="container">
+          {tag && (
             <>
+              <p className="typography--h2">
+                Posts matching <span className="tag">{tag}</span>
+              </p>
+
+              <p className="typography--h3">
+                {posts.length} {posts.length > 1 ? "results" : "result"}
+              </p>
+
+              <hr />
+            </>
+          )}
+
+          {posts
+            ?.slice(0, 1)
+            .map(({ title, slug, date, abstract, tags, image }) => (
+              <>
+                <ArticleTeaser
+                  key={slug}
+                  title={title}
+                  date={date}
+                  slug={slug}
+                  abstract={abstract}
+                  tags={tags}
+                  image={image}
+                />
+              </>
+            ))}
+
+          {!tag && (
+            <>
+              <div className="tags-list">
+                <h3>Topics</h3>
+                <p>
+                  {tags &&
+                    Object.entries(tags).map(([tag, count]) => (
+                      <Link
+                        key={tag}
+                        href={`/blog?tag=${encodeURIComponent(
+                          tag
+                        ).toLowerCase()}`}
+                        className="tag"
+                      >
+                        <>
+                          {tag} <span className="tag_count">{count}</span>
+                        </>
+                      </Link>
+                    ))}
+                </p>
+              </div>
+
+              <BlogMessage />
+            </>
+          )}
+
+          {posts
+            ?.slice(1, 5)
+            .map(({ title, slug, date, abstract, tags, image }) => (
               <ArticleTeaser
                 key={slug}
                 title={title}
@@ -49,88 +91,51 @@ const Blog: FunctionComponent<Props> = ({ posts, tag, tags }) => {
                 tags={tags}
                 image={image}
               />
+            ))}
+        </div>
+
+        <div className="container container--large">
+          <ul className="posts__list">
+            {posts?.slice(5, -1).map(({ title, slug, date, image }) => (
+              <ArticleTeaserSmall
+                key={slug}
+                title={title}
+                date={date}
+                slug={slug}
+                image={image}
+              />
+            ))}
+          </ul>
+
+          {!tag && (
+            <>
+              <h3>On other websites</h3>
+
+              <ul className="posts__list posts__list--no-top">
+                <ArticleTeaserSmall
+                  title="CSS Naming Conventions: Less Rules, more Fun on Medium.com"
+                  url="https://medium.com/@drublic/css-naming-conventions-less-rules-more-fun-12af220e949b"
+                  date="19.06.2014"
+                  description=""
+                />
+
+                <ArticleTeaserSmall
+                  title="A counter statement: Putting the CSS in the head on Medium.com"
+                  url="https://medium.com/programming-ideas-tutorial-and-experience/f98103d09ce1"
+                  date="03.02.2014"
+                  description=""
+                />
+
+                <ArticleTeaserSmall
+                  title="Shared Grunt Configuration for The Nitty Gritty"
+                  url="https://thenittygritty.co/shared-grunt-configuration"
+                  date="09.06.2013"
+                  description=""
+                />
+              </ul>
             </>
-          ))}
-
-        {!tag && (
-          <>
-            <div className="tags-list">
-              <h3>Topics</h3>
-              <p>
-                {tags &&
-                  Object.entries(tags).map(([tag, count]) => (
-                    <Link
-                      key={tag}
-                      href={`/blog?tag=${encodeURIComponent(
-                        tag
-                      ).toLowerCase()}`}
-                      className="tag"
-                    >
-                      <>
-                        {tag} <span className="tag_count">{count}</span>
-                      </>
-                    </Link>
-                  ))}
-              </p>
-            </div>
-
-            <BlogMessage />
-          </>
-        )}
-
-        {posts
-          ?.slice(1, 5)
-          .map(({ title, slug, date, abstract, tags, image }) => (
-            <ArticleTeaser
-              key={slug}
-              title={title}
-              date={date}
-              slug={slug}
-              abstract={abstract}
-              tags={tags}
-              image={image}
-            />
-          ))}
-
-        <ul className="posts__list">
-          {posts?.slice(5, -1).map(({ title, slug, date }) => (
-            <ArticleTeaserSmall
-              key={slug}
-              title={title}
-              date={date}
-              slug={slug}
-            />
-          ))}
-        </ul>
-
-        {!tag && (
-          <>
-            <h3>On other websites</h3>
-
-            <ul className="posts__list posts__list--no-top">
-              <ArticleTeaserSmall
-                title="CSS Naming Conventions: Less Rules, more Fun on Medium.com"
-                url="https://medium.com/@drublic/css-naming-conventions-less-rules-more-fun-12af220e949b"
-                date="19.06.2014"
-                description=""
-              />
-
-              <ArticleTeaserSmall
-                title="A counter statement: Putting the CSS in the head on Medium.com"
-                url="https://medium.com/programming-ideas-tutorial-and-experience/f98103d09ce1"
-                date="03.02.2014"
-                description=""
-              />
-
-              <ArticleTeaserSmall
-                title="Shared Grunt Configuration for The Nitty Gritty"
-                url="https://thenittygritty.co/shared-grunt-configuration"
-                date="09.06.2013"
-                description=""
-              />
-            </ul>
-          </>
-        )}
+          )}
+        </div>
       </main>
     </Layout>
   );
