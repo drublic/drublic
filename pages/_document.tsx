aa74b44f1598f7f6c5c78f82341c828dd83f4ea7
Hans Christian Reinl <info@drublic.de>
2023-03-01 07:35:07 +0100

    Fix font file from google fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ export default function Document() {
           crossOrigin=""
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@900&family=Roboto:wght@300,700&family=Vollkorn:ital@1&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@900&family=Roboto:wght@300;700&family=Vollkorn:ital@1&display=swap"
           rel="stylesheet"
         />
       </Head>
