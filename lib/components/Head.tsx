b48a668ec26dd8728dd054067cac6285f392c603
Hans Christian Reinl <info@drublic.de>
2024-06-03 15:36:59 +0200

    Add canonical url to header

--- lib/components/Head.tsx
+++ lib/components/Head.tsx
@@ -1,65 +1,75 @@
 import React from "react";
 import NextHead from "next/head";
+import { usePathname } from "next/navigation";
 
-const Head = ({ title, description, image, noIndex = false }) => (
-  <NextHead>
-    <meta charSet="utf-8" />
+const Head = ({ title, description, image, noIndex = false }) => {
+  const pathname = usePathname();
 
-    <title>{title}</title>
+  return (
+    <NextHead>
+      <meta charSet="utf-8" />
 
-    <link rel="dns-prefetch" href="//ajax.googleapis.com/" />
-    <link rel="dns-prefetch" href="//www.google-analytics.com/" />
-    <meta name="description" content={description} />
-    {noIndex && <meta name="robots" content="noindex" />}
+      <title>{title}</title>
 
-    <link
-      rel="alternate"
-      type="application/rss+xml"
-      title="drublic Blog"
-      href="/feed"
-    />
+      <link rel="dns-prefetch" href="//ajax.googleapis.com/" />
+      <link rel="dns-prefetch" href="//www.google-analytics.com/" />
+      <meta name="description" content={description} />
+      {noIndex && <meta name="robots" content="noindex" />}
 
-    <meta property="twitter:domain" content="hansreinl.de" />
-    <meta name="twitter:site" content="@drublic" />
-    <meta name="twitter:title" content={title} />
-    <meta name="twitter:description" content={description} />
+      <link
+        rel="alternate"
+        type="application/rss+xml"
+        title="drublic Blog"
+        href="/feed"
+      />
 
-    <meta property="og:type" content="website" />
-    <meta property="og:title" content={title} />
-    <meta property="og:description" content={description} />
-    {image ? (
-      <>
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content={image} />
-        <meta property="og:image" content={image} />
-      </>
-    ) : (
-      <>
-        <meta name="twitter:card" content="summary" />
-        <meta
-          name="twitter:image"
-          content="https://hansreinl.de/img/logo.png"
-        />
-        <meta property="og:image" content="https://hansreinl.de/img/logo.png" />
-      </>
-    )}
+      <meta property="twitter:domain" content="hansreinl.de" />
+      <meta name="twitter:site" content="@drublic" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
 
-    <link type="text/plain" rel="author" href="/humans.txt" />
-    <meta name="author" content="Hans Christian Reinl" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      {image ? (
+        <>
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:image" content={image} />
+          <meta property="og:image" content={image} />
+        </>
+      ) : (
+        <>
+          <meta name="twitter:card" content="summary" />
+          <meta
+            name="twitter:image"
+            content="https://hansreinl.de/img/logo.png"
+          />
+          <meta
+            property="og:image"
+            content="https://hansreinl.de/img/logo.png"
+          />
+        </>
+      )}
 
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link type="text/plain" rel="author" href="/humans.txt" />
+      <meta name="author" content="Hans Christian Reinl" />
 
-    <meta name="application-name" content="drublic" />
-    <meta name="theme-color" content="#316eae" />
-    <link rel="manifest" href="/manifest.json" />
-    <link rel="shortcut icon" href="/favicon.png" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <link
-      rel="index"
-      href="/"
-      title="drublic - State of the Art Webdesign - HTML, CSS, JavaScript - Hans Christian Reinl"
-    />
-  </NextHead>
-);
+      <meta name="application-name" content="drublic" />
+      <meta name="theme-color" content="#316eae" />
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="shortcut icon" href="/favicon.png" />
+
+      <link
+        rel="index"
+        href="/"
+        title="drublic - State of the Art Webdesign - HTML, CSS, JavaScript - Hans Christian Reinl"
+      />
+
+      <link rel="canonical" href={`https://hansreinl.de${pathname}`} />
+    </NextHead>
+  );
+};
 
 export default Head;
