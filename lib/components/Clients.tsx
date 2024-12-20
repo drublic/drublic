ff1462c0d374cb27c7a7ae1c451b8ea02d439631
Hans Christian Reinl <info@drublic.de>
2024-12-20 18:49:31 +0100

    Update clients list

--- lib/components/Clients.tsx
+++ lib/components/Clients.tsx
@@ -19,6 +19,18 @@ const Clients = () => (
     </li>
 
     <li className="work__clients__client work__clients__client--large">
+      <a href="http://www.prosiebensat1.de/">
+        <img src="/img/work/prosiebensat1.jpg" alt="ProSiebenSat.1" />
+      </a>
+    </li>
+
+    <li className="work__clients__client work__clients__client--large">
+      <a href="http://www.sapdatacenter.com/">
+        <img src="/img/work/sap.svg" alt="SAP AG" />
+      </a>
+    </li>
+
+    <li className="work__clients__client work__clients__client--large">
       <a href="https://axa.de/">
         <img
           src="/img/work/axa.svg"
@@ -29,8 +41,8 @@ const Clients = () => (
     </li>
 
     <li className="work__clients__client work__clients__client--large">
-      <a href="http://www.sapdatacenter.com/">
-        <img src="/img/work/sap.svg" alt="SAP AG" />
+      <a href="http://www.joyn.de/">
+        <img src="/img/work/joyn.svg" alt="Joyn" />
       </a>
     </li>
 
@@ -41,22 +53,7 @@ const Clients = () => (
     </li>
 
     <li className="work__clients__client">
-      <a href="http://vaillant-group.com/">
-        <img src="/img/work/sd.png" alt="Saunier Duval" />
-      </a>
-    </li>
-
-    <li className="work__clients__client">
-      <a href="http://www.fashionid.de/">
-        <img
-          src="/img/work/fid.png"
-          alt="Fashion ID by Peek &amp; Cloppenburg"
-        />
-      </a>
-    </li>
-
-    <li className="work__clients__client">
-      <a href="https://www.fashionette.pl/">
+      <a href="https://www.fashionette.de/">
         <img src="/img/work/fashionette.svg" alt="Fashionette" />
       </a>
     </li>
@@ -68,7 +65,7 @@ const Clients = () => (
     </li>
 
     <li className="work__clients__client">
-      <a href="https://www.shareacamper.co.nz/en-nz/">
+      <a href="https://www.yescapa.de/yescapa-uebernimmt-shareacamper/">
         <img src="/img/work/shareacamper.svg" alt="Share A Camper" />
       </a>
     </li>
